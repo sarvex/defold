a2de75ec61de0690944a19f223155baac0879338
Jhonny Göransson <jhonny.goransson@gmail.com>
2022-12-19 08:15:08 +0100

    Configure stb_iamge for hdr/linear

--- engine/dlib/src/dlib/image.cpp
+++ engine/dlib/src/dlib/image.cpp
@@ -22,11 +22,10 @@
 #define STBI_NO_PSD
 #define STBI_NO_TGA
 #define STBI_NO_GIF
-#define STBI_NO_HDR
+//#define STBI_NO_HDR
 #define STBI_NO_PIC
 #define STBI_NO_PNM
-#define STBI_NO_HDR
-#define STBI_NO_LINEAR
+//#define STBI_NO_LINEAR
 #define STBI_NO_STDIO
 #define STBI_FAILURE_USERMSG
 #define STB_IMAGE_IMPLEMENTATION
